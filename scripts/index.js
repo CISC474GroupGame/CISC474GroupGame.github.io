e4806eac161900dcf706f55c4ad8c5b491d0444b
Collin Reich <cdreich7@gmail.com>
2021-09-22 17:59:08 -0400

    game loop running and animation moving across the screen
    merge

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 $(document).ready(function(){
 	window.addEventListener("keydown", function (event) { //https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/key#example
 		if (event.defaultPrevented) {
@@ -88,6 +89,55 @@ $(document).ready(function(){
 // playerAvatar.style.height = playerData.height;
 
 $(document).ready(function(){
+=======
+// $(document).ready(function(){
+// 	window.addEventListener("keydown", function (event) { //https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/key#example
+// 		if (event.defaultPrevented) {
+// 			return; // Do nothing if the event was already processed
+// 		}			
+// 		switch (event.key) {
+// 			case "Down": // IE/Edge specific value
+// 			case "ArrowDown":
+// 			$("#square").css("top", "+=5");
+// 			// Do something for "down arrow" key press.
+// 			break;
+// 			case "Up": // IE/Edge specific value
+// 			case "ArrowUp":
+// 			$("#square").css("top", "-=5");
+// 			// Do something for "up arrow" key press.
+// 			break;
+// 			case "Left": // IE/Edge specific value
+// 			case "ArrowLeft":
+// 			$("#square").css("left", "-=5");
+// 			// Do something for "left arrow" key press.
+// 			break;
+// 			case "Right": // IE/Edge specific value
+// 			case "ArrowRight":
+// 			$("#square").css("left", "+=5");
+// 			// Do something for "right arrow" key press.
+// 			break;
+// 			case "Enter":
+// 			// Do something for "enter" or "return" key press.
+// 			break;
+// 			case "Esc": // IE/Edge specific value
+// 			case "Escape":
+// 			// Do something for "esc" key press.
+// 			break;
+// 			default:
+// 			return; // Quit when this doesn't handle the key event.
+// 		}
+
+// 		// Cancel the default action to avoid it being handled twice
+// 		event.preventDefault();
+// 		}, true);
+// })
+
+// let playerData = new Player(400, 400, 0, 0);
+// playerAvatar.style.width = playerData.width;
+// playerAvatar.style.height = playerData.height;
+
+$(document).ready(function(){
+>>>>>>> 2dda061 (game loop running and animation moving across the screen)
 
 	// let gameCanvas = document.getElementById("gameCanvas");
 	// let playerAvatar = document.getElementById("playerAvatar");
@@ -196,5 +246,9 @@ function easeInOutQuint (t, b, c, d) {
 
 function easeLinear (t, b, c, d) {
     return c * t / d + b;
+<<<<<<< HEAD
 }
 >>>>>>> JD
+=======
+}
+>>>>>>> 2dda061 (game loop running and animation moving across the screen)
