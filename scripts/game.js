41ecbd142533c31996c16293d994697aad37f1f9
Collin Reich <cdreich7@gmail.com>
2021-11-16 07:33:08 -0500

    cleaned out old code and added respawn feature

--- scripts/game.js
+++ scripts/game.js
@@ -1,122 +1,112 @@
-//global attributes -- maybe this file should be a class??
-let canvas;
-let context;
-let currentLevel;
-let playerModel;
-let gameObjects;
+//creating frames
+(function () {
+    let requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+    window.requestAnimationFrame = requestAnimationFrame;
+})();
 
-let init = function () {
-	canvas = document.getElementById("gameCanvas");
-    context = canvas.getContext("2d");
-	window.addEventListener("resize", resizeCanvas, false);
-	resizeCanvas();
-	currentLevel = new levelOne(canvas, context);
-    playerModel = currentLevel.levelData.playerModel;
-	gameObjects = currentLevel.levelData.allGameObjects;
-	window.requestAnimationFrame(gameLoop);
+//setting up key tracking
+let trackKeys = function(keys){
+    let down = Object.create(null);
+	function track(event) {
+		if (keys.includes(event.key)) {
+			down[event.key] = event.type == "keydown";
+			event.preventDefault();
+		}
+	}
+	window.addEventListener("keydown", track);
+	window.addEventListener("keyup", track);
+	return down;
 }
+let arrowKeys = trackKeys(["ArrowLeft", "ArrowRight", "ArrowUp", "ArrowDown"]);
 
+//initialize the game when the window loads
+let canvas, context, currentLevel, player, playerSpawnState;
+let init = function(){
+    canvas = document.getElementById('gameCanvas');
+    context = canvas.getContext("2d");
+    window.addEventListener("resize", resizeCanvas, false);
+    resizeCanvas();
+    levelData = loadLevelOne(canvas, context);
+    player = levelData.player;
+    playerSpawnState = Object.assign({}, levelData.player);
+    update();
+}
 window.onload = init;
 
+//makes canvas size responsive to screen size
 let resizeCanvas = function () {
 	canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
 }
 
-let secondsPassed = 0;
-let oldTimeStamp = 0;
-let timePassed = 0;
+//main driver function for the app (gets called every frame)
+//can modulize this and break it down into other functions to be cleaner
+let update = function() {
 
-let gameLoop = function (timestamp) {
-	secondsPassed = (timestamp - oldTimeStamp) / 1000;
-	oldTimeStamp = timestamp;
+    fallCheck();
 
-	for(let i = 0; i < gameObjects.length; i++){
-		gameObjects[i].update(secondsPassed, arrowKeys);
-	}
-
-	context.clearRect(0, 0, canvas.width, canvas.height);
-
-	for(let i = 0; i < gameObjects.length; i++){
-		gameObjects[i].draw();
-	}
-
-	for(let i = 0; i < currentLevel.levelData.platforms.length; i++){
-		let collisionDirection = collisionCheck(playerModel, currentLevel.levelData.platforms);
-		if (collisionDirection === "left" || collisionDirection === "right") {
-            playerModel.velX = 0;
-            playerModel.jumping = false;
-        } else if (collisionDirection === "bottom") {
-            playerModel.grounded = true;
-            playerModel.jumping = false;
-        } else if (collisionDirection === "top") {
-            playerModel.velY *= -1;
+    //player movement
+    if(arrowKeys.ArrowUp){
+        if(!player.jumping && player.grounded){
+            player.jumping = true;
+            player.grounded = false;
+            player.vy = -player.speed * 2.5;
         }
-	}
-	detectCollision()
-
-	window.requestAnimationFrame(gameLoop);
-}
-
-let rectIntersect = function(playerX, playerY, playerWidth, playerHeight, objectX, objectY, objectWidth, objectHeight){
-	// if (x2 > w1 + x1){
-	// 	console.log("x2");
-	// }
-	// if (x1 > w2 + x2){
-	// 	console.log("x1");
-	// }
-	// if (y2 > h1 + y1){
-	// 	console.log("y2");
-	// }
-	// if (y1 > h2 + y2){
-	// 	console.log("y1");
-	// }
-	if (objectX > playerWidth + playerX || playerX > objectWidth + objectX || objectY > playerHeight + playerY || playerY > objectHeight + objectY){
-		// console.log("rectangles not colliding");
-        return false;
     }
-	else{
-		// console.log("rectangles colliding");
-    	return true;
-	}
-	// console.log("rectangles colliding");
-    // return true;
+    if(arrowKeys.ArrowLeft){
+        if(player.vx > -player.speed){
+            player.vx = player.vx - 1;
+        }
+    }
+    if(arrowKeys.ArrowRight){
+        if(player.vx < player.speed){
+            player.vx = player.vx + 1;
+        }
+    }
+    player.vx *= player.friction;
+    player.vy += player.gravity;
+
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.beginPath();
+
+    player.grounded = false;
+
+    //render platforms and check collision with player
+    for(let i = 0; i < levelData.platforms.length; i++){
+        let currentPlatform = levelData.platforms[i];
+        context.fillStyle = '#000000';
+        context.rect(currentPlatform.x, currentPlatform.y, currentPlatform.width, currentPlatform.height);
+        let collisionDirection = collisionCheck(player, currentPlatform);
+        if (collisionDirection === "left" || collisionDirection === "right") {
+            player.vx = 0;
+            player.jumping = false;
+        } else if (collisionDirection === "bottom") {
+            player.grounded = true;
+            player.jumping = false;
+        } else if (collisionDirection === "top") {
+            player.vy *= -1;
+        }
+    }
+
+    //stop player from moving if they become grounded
+    if(player.grounded){
+        player.vy = 0;
+    }
+
+    //move actual player position based on velocity
+    player.x += player.vx;
+    player.y += player.vy;
+
+    //canvas animation updating
+    context.fill();
+    context.fillStyle = '#0099b0';
+    context.fillRect(player.x, player.y, player.width, player.height);
+
+
+    requestAnimationFrame(update);
 }
 
-let detectCollision = function(){
-	let gameObject;
-
-	//Reset collision state on all objects
-	for(let i = 0; i < gameObjects.length; i++){
-		gameObjects[i].isColliding = false;
-	}
-
-	//Check for collisions
-	for(let i = 1; i < gameObjects.length; i++){
-		//we need to start at 1 because the playerModel is the first gameObject
-		gameObject = gameObjects[i];
-
-		if(rectIntersect(playerModel.x, playerModel.y, playerModel.width, playerModel.height, gameObject.x, gameObject.y, gameObject.width, gameObject.height)){
-			// let vCollision = {x: obj2.x - obj1.x, y: obj2.y - obj1.y};
-			// let distance = Math.sqrt((obj2.x-obj1.x)*(obj2.x-obj1.x) + (obj2.y-obj1.y)*(obj2.y-obj1.y));
-			// let vCollisionNorm = {x: vCollision.x / distance, y: vCollision.y / distance};
-			// let vRelativeVelocity = {x: obj1.vx - obj2.vx, y: obj1.vy - obj2.vy};
-			// let speed = vRelativeVelocity.x * vCollisionNorm.x + vRelativeVelocity.y * vCollisionNorm.y;
-			// if (speed < 0){
-			// 	break;
-			// }
-			// obj1.vx -= (speed * vCollisionNorm.x);
-			// obj1.vy -= (speed * vCollisionNorm.y);
-			// obj2.vx += (speed * vCollisionNorm.x);
-			// obj2.vy += (speed * vCollisionNorm.y);
-
-			playerModel.isColliding = true;
-			gameObject.isColliding = true;
-		}
-	}
-
-}
-
+//function for detecting collision between two given objects
 let collisionCheck = function(obj1, obj2){
 	let vectorX = (obj1.x + (obj1.width / 2)) - (obj2.x + (obj2.width / 2));
 	let vectorY = (obj1.y + (obj1.height / 2)) - (obj2.y + (obj2.height / 2));
@@ -135,7 +125,7 @@ let collisionCheck = function(obj1, obj2){
                 obj1.y -= oY;
             }
         } else {
-            if (vX > 0) {
+            if (vectorX > 0) {
                 collisionDirection = "left";
                 obj1.x += oX;
             } else {
@@ -147,17 +137,10 @@ let collisionCheck = function(obj1, obj2){
 	return collisionDirection;
 }
 
-let trackKeys = function(keys){
-    let down = Object.create(null);
-	function track(event) {
-		if (keys.includes(event.key)) {
-			down[event.key] = event.type == "keydown";
-			event.preventDefault();
-		}
-	}
-	window.addEventListener("keydown", track);
-	window.addEventListener("keyup", track);
-	return down;
+//if player falls out of the screen, respawns
+let fallCheck = function(){
+    if(player.y > canvas.height + 500){
+        player = levelData.player = playerSpawnState;
+        playerSpawnState = Object.assign({}, levelData.player);
+    }
 }
-
-let arrowKeys = trackKeys(["ArrowLeft", "ArrowRight", "ArrowUp", "ArrowDown"]);
--- scripts/gameElements/BasicPlatform.js
+++ scripts/gameElements/BasicPlatform.js
@@ -2,13 +2,4 @@ class BasicPlatform extends PlatformObject{
     constructor(context, x, y, width, height){
         super(context, x, y, width, height);
     }
-
-    draw(){
-        this.context.fillStyle = '#000000';
-        this.context.rect(this.x, this.y, this.width, this.height);
-    }
-
-    update(){
-
-    }
 }
