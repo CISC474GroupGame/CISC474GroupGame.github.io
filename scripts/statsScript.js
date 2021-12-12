a974e91c5cc284728e30b9038207ce05af3d317a
Collin Reich <cdreich7@gmail.com>
2021-12-12 16:42:41 -0500

    addition of stats page before api endpoint implemented

---
+++ scripts/statsScript.js
@@ -0,0 +1,21 @@
+window.onload = load;
+
+function load(){
+    let statsObject = { //this will be populated from the API endpoint and default to 0
+        highscore: 0,
+        time: 0,
+        resets: 0,
+        deaths: 0,
+        coins: 0,
+        jumps: 0,
+        distance: 0,
+    }
+    
+    document.getElementById('highscore').innerText = "Highscore: " + statsObject.highscore;
+    document.getElementById('total-time').innerText = "Time Played: " + statsObject.time;
+    document.getElementById('total-resets').innerText = "Total Resets: " + statsObject.resets;
+    document.getElementById('total-deaths').innerText = "Total Deaths: " + statsObject.deaths;
+    document.getElementById('total-coins').innerText = "Total Coins: " + statsObject.coins;
+    document.getElementById('total-jumps').innerText = "Total Jumps: " + statsObject.jumps;
+    document.getElementById('total-distance').innerText = "Distance Travelled: " + statsObject.distance;
+}
